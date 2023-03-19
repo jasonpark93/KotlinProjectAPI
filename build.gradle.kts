772f8f2b12d53466acb7c22f82f9a0a103116539
jasonpark93 <qkrwotydtyd@gmail.com>
2023-03-20 02:24:40 +0900

    readme

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,17 +3,43 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.8.10"
     application
+    id("org.springframework.boot") version "2.5.5"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("plugin.spring") version "1.5.31"
+    kotlin("plugin.jpa") version "1.5.31"
 }
 
 group = "me.kakao"
 version = "1.0-SNAPSHOT"
 
+application {
+    mainClass.set("WebFluxApplicationKt")
+}
+
+
 repositories {
     mavenCentral()
+    maven {
+        url = uri("https://jitpack.io")
+    }
 }
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("io.projectreactor:reactor-core")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.json:json:20200518")
+    implementation("io.r2dbc:r2dbc-h2")
+    implementation("io.r2dbc:r2dbc-pool")
+    implementation("com.h2database:h2")
+    implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
 }
 
 tasks.test {
